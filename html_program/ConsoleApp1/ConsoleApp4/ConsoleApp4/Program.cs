216a2446afb95dedfd95a071156a23c999373846
MANISH PATHAK <anjalipathak1221@gmail.com>
2023-03-16 17:18:34 +0530

    file added

---
+++ html_program/ConsoleApp1/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2,num3,sum;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            sum =num1+ num2+ num3;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
