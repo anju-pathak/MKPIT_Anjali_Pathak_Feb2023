603e9c71229788716c87d53ab5ab2c550d87d6e0
MANISH PATHAK <anjalipathak1221@gmail.com>
2023-03-27 10:44:57 +0530

    file added

---
+++ add.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            char op;
+            int res = 0;
+            Console.WriteLine("enter num1");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter num2");
+            num2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter operator like + - *");
+            op=Convert.ToChar(Console.ReadLine());
+            if (op == '+')
+                res = num1 + num2;
+            else if (op == '-')
+                res = num1 - num2;
+            else if (op == '*')
+                res = num1 * num2;
+            else
+                Console.WriteLine("invalid");
+                Console.ReadKey();
+            }
+        }
+            
+    }
+
---
+++ hg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i= 1;i<=10;i++)
+                {
+                if (i==5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+                Console.ReadKey();
+                }
+
+        }
+    }
+}
