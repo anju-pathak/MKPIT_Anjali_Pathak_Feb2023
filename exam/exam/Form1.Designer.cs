41af69ed55d32bcca7e2668bbaab3fa56594533e
MANISH PATHAK <anjalipathak1221@gmail.com>
2023-03-28 10:30:17 +0530

    file added

---
+++ exam/exam/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(882, 488);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ exam1/exam1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exam1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int total = 0;
+            if (radioButton1.Checked)
+            {
+                total = total + 10;
+            }
+            if (radioButton5.Checked)
+            {
+                total = total + 10;
+            }
+            if (radioButton7.Checked)
+            {
+                total = total + 10;
+            }
+            int counter = 0;
+            if (checkBox1.Checked)
+            {
+                counter = counter + 1;
+                if (counter < 3)
+                {
+                    total = total + 5;
+                }
+                else
+                {
+                    MessageBox.Show("please select only 2 options");
+                }
+            }
+            if (checkBox2.Checked)
+            {
+                counter = counter + 1;
+                if (counter < 3)
+                {
+                    total = total - 5;
+                }
+                else
+                {
+                    MessageBox.Show("please select only 2 options");
+                }
+            }
+            if (checkBox3.Checked)
+            {
+                counter = counter + 1;
+                if (counter < 3)
+                {
+                    total = total + 5;
+                }
+                else
+
+                {
+                    MessageBox.Show("please select only 2 options");
+                }
+            }
+            label4.Text = "total out of 50" + total;
+        }
+    }
+}
+
+
+        
