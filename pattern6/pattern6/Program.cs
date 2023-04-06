f224ca48f202cb8a93bc54c89c54009ec322caf1
MANISH PATHAK <anjalipathak1221@gmail.com>
2023-04-06 15:45:07 +0530

    file added

---
+++ pattern6/pattern6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=3;i++)
+            {
+                for(int  j=1; j<=i;j++)
+                {
+                    for(int k=1; k<=2*i-1;k++)
+                    {
+                        Console.Write(j);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
