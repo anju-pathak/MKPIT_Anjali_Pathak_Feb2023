954571376c1ac8763dd51973ae36c019bdc55f74
MANISH PATHAK <anjalipathak1221@gmail.com>
2023-04-26 15:37:30 +0530

    file added

---
+++ exceptation handling/exceptation handling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptation_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2=0;
+            int res = 0;
+            try
+            {
+                res = n1 / n2;
+                Console.WriteLine("result=" + res);
+            }
+            catch (Exception  ee) //ee is object of exception class
+            {
+                Console.WriteLine(ee.ToString());
+                   
+            }
+
+        }
+    }
+}
---
+++ exception1/exception1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = 2;
+            int res = 0;
+            try
+            {
+                res = n1 / n2;
+                Console.WriteLine("result" + res);
+            }
+            catch (DivideByZeroException ee)
+            {
+                Console.WriteLine(ee.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("final executed");
+            }
+            Console.WriteLine("bye");
+
+        }
+    }
+}
+        
+    
+
